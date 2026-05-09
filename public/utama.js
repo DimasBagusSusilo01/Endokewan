8291de88987e4a18e15665c9d0e948700e832f44
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-09 08:45:58 +0100

    update public/utama.js

--- public/utama.js
+++ public/utama.js
@@ -3,3 +3,24 @@ terminal = document.getElementById('login');
 tombol.addEventListener('click', function(){
     terminal.classList.toggle('aktif');
 });
+
+utama = document.getElementById('utama');
+kotakTentang = document.getElementById('kotakTentang');
+kembali = document.getElementById('kembali');
+tentang = document.getElementById('tentang');
+
+tentang.addEventListener('click', function(){
+  utama.classList.add('hilang');
+  utama.classList.remove('muncul');
+  
+  kotakTentang.classList.add('muncul');
+  kotakTentang.classList.remove('hilang');
+});
+
+kembali.addEventListener('click', function(){
+  utama.classList.remove('hilang');
+  utama.classList.add('muncul');
+  
+  kotakTentang.classList.remove('muncul');
+  kotakTentang.classList.add('hilang');
+});
