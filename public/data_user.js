3b53b9f93ea4a058dffa8d6c1f2ff0a42b17bac3
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-11 12:02:56 +0100

    update public/data_user.js

--- public/data_user.js
+++ public/data_user.js
@@ -1,3 +1,4 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const SUPABASE_URL = 'https://tlmidazvewettxhlwbvx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
