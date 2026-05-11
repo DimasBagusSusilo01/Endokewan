25b5c7273a6f53b6f784f47e9acc9a0c04925ba3
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-11 13:43:34 +0100

    update public/data_user.js

--- public/data_user.js
+++ public/data_user.js
@@ -1,5 +1,5 @@
 const supabase = window.supabase.createClient(
-  'https://tim1dazvewettxhlwbvx.supabase.co',
+  'https://tlmidazvewettxhlwbvx.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM'
 );
 
