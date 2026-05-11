279a7241c0b52c967a60c9736bfa0ead6bb290ed
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-11 11:57:35 +0100

    update public/data_user.js

--- public/data_user.js
+++ public/data_user.js
@@ -1,11 +1,11 @@
 const SUPABASE_URL = 'https://tlmidazvewettxhlwbvx.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM';
 
-const nama = document.getElementById('nama').value;
-kirimnama = document.getElementById('kirimnama');
+const kirimnama = document.getElementById('kirimnama');
 
 kirimnama.addEventListener('click', async function(){
   const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  const nama = document.getElementById('nama').value;
   const {
     data: { user }
   } = await supabase.auth.getUser();
@@ -14,7 +14,7 @@ kirimnama.addEventListener('click', async function(){
 
   await supabase
     .from('users')
-    .insert({
+    .upsert({
       id: user.id,
       email: user.email,
       nama: nama,
