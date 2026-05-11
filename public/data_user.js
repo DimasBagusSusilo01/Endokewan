3d43aff91381123272066766999fcba8ed8b6346
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-11 14:24:26 +0100

    update public/data_user.js

--- public/data_user.js
+++ public/data_user.js
@@ -1,4 +1,17 @@
-const supabase = window.supabase.createClient('https://tlmidazvewettxhlwbvx.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM');
+const supabase = window.supabase.createClient('https://tlmidazvewettxhlwbvx.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM'),{
+          auth: {
+
+            // penting untuk google oauth
+            flowType: 'pkce',
+
+            // baca ?code= otomatis
+            detectSessionInUrl: true,
+
+            // simpan session
+            persistSession: true
+
+          }
+        };
 
 let currentSession = null;
 
