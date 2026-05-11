d6c6dd0769e8119f6eba45c782e70cdfb8bff1b4
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-11 13:01:11 +0100

    update public/data_user.js serah

--- public/data_user.js
+++ public/data_user.js
@@ -56,8 +56,6 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
   });
 
 });
-
-</script>
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
