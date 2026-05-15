0ae46886ea756951c1a1b33a4d852780a6af6808
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-15 15:18:40 +0100

    tambahan ui ketik

---
+++ public/ubah_ui.js
@@ -0,0 +1,23 @@
+const ketik = document.getElementById('ketik');
+const pribadi = document.getElementById('pribadi');
+const publik = document.getElementById('publik');
+const kirim = document.getElementById('kirim_ide');
+
+ui = document.getElementById('ui');
+sesiketik = document.getElementById('sesiketik');
+
+ketik.addEventListener('click', function(){
+    sesiketik.classList.add('muncul');
+    sesiketik.classList.remove('hilang');
+
+    ui.classList.add('hilang');
+    ui.classList.remove('muncul');
+});
+
+kembali.addEventListener('click', function(){
+    ui.classList.add('muncul');
+    ui.classList.remove('hilang');
+
+    sesiketik.classList.add('hilang');
+    sesiketik.classList.remove('muncul');
+});
