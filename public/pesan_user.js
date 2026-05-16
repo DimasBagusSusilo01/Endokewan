fe8aeb95bd162a48de38258b1d7055db4be7bc82
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-05-16 13:48:05 +0100

    nambah pengait antar database

---
+++ public/pesan_user.js
@@ -0,0 +1,55 @@
+const SUPABASE_URL =
+      "https://tlmidazvewettxhlwbvx.supabase.co";
+const SUPABASE_ANON_KEY =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsbWlkYXp2ZXdldHR4aGx3YnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjkyNjEsImV4cCI6MjA5MzkwNTI2MX0.iAiayUK-H1TppKLSdLDF1ugNzzBZ143Z-qwqGj1CPtM";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  {
+    auth: {
+      flowType: 'pkce',
+      detectSessionInUrl: true,
+      persistSession: true
+    }
+  }
+);
+
+async function initAuth() {
+
+    await new Promise(resolve =>
+      setTimeout(resolve, 3000)
+    );
+  
+    const { data, error } =
+      await supabase.auth.getSession();
+  
+    currentSession = data.session;
+  
+    if (!currentSession) {
+      alert("Session tidak ditemukan");
+      return;
+    }
+  
+    console.log(
+      "LOGIN:",
+      currentSession.user.email
+    );
+  
+    // Ambil data pengguna
+    const {
+    data: userData,
+    error: dbError
+  } = await supabase
+    .from("DataPengguna")
+    .select(`
+    id, email, nama, status,
+    PesanPengguna(
+        nama,pesan_publik,pesan_private
+    )`);
+  
+    if (dbError) {
+      console.log(dbError);
+      return;
+    }
+}
